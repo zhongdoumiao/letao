00f4a40ce67110ec4d7f57552331abacd76ef1f4
lw <https://github.com/zhongdoumiao/study.git>
2018-01-12 19:06:17 +0800

    首页侧边栏完成

--- public/pc/js/common.js
+++ public/pc/js/common.js
@@ -2,7 +2,9 @@
  * Created by 种豆苗 on 2018/1/11.
  */
 $(function(){
-
-
-
+    //二级菜单显示隐藏功能
+    $(".aside .classify").prev().on("click",function(){
+        $(this).siblings().slideToggle();
+    });
+    //
 });
